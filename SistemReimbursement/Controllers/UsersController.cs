c859341571bcec0dec9af46324120bc9e76a82f4
Naufal Farras <naufaldotfarras@gmail.com>
2021-06-25 10:25:15 +0700

    LoginVm Update2

--- SistemReimbursement/Controllers/UsersController.cs
+++ SistemReimbursement/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SistemReimbursement.Base;
 using SistemReimbursement.Models;
 using SistemReimbursement.Repository.Data;
+using SistemReimbursement.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,40 @@ namespace SistemReimbursement.Controllers
     [ApiController]
     public class UsersController : BaseController<User, UserRepository, int>
     {
-        public UsersController(UserRepository user) : base(user) { }
+        UserRepository repo;
+        public UsersController(UserRepository user) : base(user) { 
+            this.repo = user;
+
+        }
+
+
+        [Route("Login")]
+        [HttpPost]
+        public ActionResult Login(LoginVM login)
+        {
+            var ceklogin = repo.Login(login);
+            if (ceklogin == 404)
+            {
+                return BadRequest("Email Tidak Ditemukan!");
+            }
+            else if (ceklogin == 401)
+            {
+                return BadRequest("Password Salah");
+            }
+            else if (ceklogin == 1)
+            {
+                //return Ok($"Login Berhasil \n Token : {repo.GenerateToken(loginVM)}");
+                //return Ok(new JWTokenVM { Token = repo.GenerateToken(loginVM), Messages = "Login Sukses" });
+                return Ok( "Login Sukses");
+
+
+
+            }
+            else
+            {
+                return BadRequest("Login Gagal");
+            }
+
+        }
     }
 }
