32737d6bf156b959ce99fc4b4c7933bf7e011fd6
vebytri <84784906+vebytri@users.noreply.github.com>
2021-07-02 09:44:04 +0700

    comit buat trial upload
    1

---
+++ ReimbursementFrontEnd/wwwroot/js/trial.js
@@ -0,0 +1,19 @@
+﻿document.getElementById('uploader').onsubmit = function () {
+    var formdata = new FormData(); //FormData object
+    var fileInput = document.getElementById('fileInput');
+    //Iterating through each files selected in fileInput
+    for (i = 0; i < fileInput.files.length; i++) {
+        //Appending each file to FormData object
+        formdata.append(fileInput.files[i].name, fileInput.files[i]);
+    }
+    //Creating an XMLHttpRequest and sending
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/Home/Upload');
+    xhr.send(formdata);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText);
+        }
+    }
+    return false;
+}
