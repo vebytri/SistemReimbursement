03d88b36536dd71f41d2fb0230ac57db8985edd4
vebytri <84784906+vebytri@users.noreply.github.com>
2021-07-02 14:50:24 +0700

    push trial upload fix

--- ReimbursementFrontEnd/wwwroot/js/trial.js
+++ ReimbursementFrontEnd/wwwroot/js/trial.js
@@ -1,19 +1,28 @@
-﻿document.getElementById('uploader').onsubmit = function () {
-    var formdata = new FormData(); //FormData object
-    var fileInput = document.getElementById('fileInput');
-    //Iterating through each files selected in fileInput
-    for (i = 0; i < fileInput.files.length; i++) {
-        //Appending each file to FormData object
-        formdata.append(fileInput.files[i].name, fileInput.files[i]);
-    }
-    //Creating an XMLHttpRequest and sending
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/Home/Upload');
-    xhr.send(formdata);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            alert(xhr.responseText);
+﻿function submitForm() {
+    //var formdata = new FormData($('#uploader2').get(0));
+    var fileInput = document.getElementById('file');
+   // console.log(formdata)
+   // console.log(file.files)
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append('file', file);
+    console.log(file)
+   
+
+    $.ajax({
+        //url: '/home/testupload',
+        url: '/home/testupload',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            //rendering success
+            console.log(data)
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            //rendering errors
+            console.log("2")
         }
-    }
-    return false;
+    });
 }
