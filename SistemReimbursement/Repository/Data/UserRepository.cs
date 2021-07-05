f5059ae5518a3ae26b593444f7d55ea56bfb0101
vebytri <84784906+vebytri@users.noreply.github.com>
2021-07-06 00:08:21 +0700

    Save email

--- SistemReimbursement/Repository/Data/UserRepository.cs
+++ SistemReimbursement/Repository/Data/UserRepository.cs
@@ -68,7 +68,7 @@ namespace SistemReimbursement.Repository.Data
                 result = conn.SaveChanges();
 
                 var message = new MimeMessage();
-                message.From.Add(new MailboxAddress("admin@reimbursement", "hai.infodigital@gmail.com"));
+                message.From.Add(new MailboxAddress("admin@reimbursement", "diancoeg1234@gmail.com"));
                 message.To.Add(new MailboxAddress($"{register.FirstName}", $"{register.Email}"));
                 message.Subject = "Sucsess Registration Account";
                 message.Body = new TextPart("plain")
@@ -80,7 +80,7 @@ namespace SistemReimbursement.Repository.Data
                 using (var client = new SmtpClient())
                 {
                     client.Connect("smtp.gmail.com", 587, false);
-                    client.Authenticate("hai.infodigital@gmail.com", "#Naufal1998");
+                    client.Authenticate("diancoeg1234@gmail.com", "diancoeg112");
                     client.Send(message);
                     client.Disconnect(true);
                 }
